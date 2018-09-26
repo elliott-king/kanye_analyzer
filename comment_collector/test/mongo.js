7eb7c3273d5e730fa8d19341a492654f256751cd
elliott-king <life.rider09@gmail.com>
2018-09-26 01:16:21 +0000

    Abstracting Mongo util fns

---
+++ comment_collector/test/mongo.js
@@ -0,0 +1,115 @@
+var mongoHandler = require('../utils/mongo_handler.js')
+var assert = require('assert');
+
+const not_wavy = {
+	data: {
+		author: 'test_author_for_node',
+		name: 'test_name_for_node',
+		body: 'does not contain the \'w\' word'
+	}
+};
+
+const exists_in_db = {
+	data: {
+		author: 'test_author_for_node',
+		name: 'test_name_for_node',
+		body: 'does contain \'wavy\''
+	}
+};
+const is_wavy = {
+	data: {
+		author: 'test_author_for_node',
+		name: 'test_name_new_in_db',
+		body: 'does contain wavy'
+	}
+};
+	
+// first time using mocha
+describe('Array', function() {
+	describe('#indexOf()', function() {
+		it('should return -1 when the value is not present', function() {
+			assert.equal([1,2,3].indexOf(4), -1);
+		});
+	});
+});
+
+describe('MongoHandler', function() {
+	var MongoClient = require('mongodb').MongoClient
+		, format = require('util').format
+		, mongoPort = '27017'
+		, collectionName = 'test'
+		, dbname = 'test';
+
+	before('add wavy comment to db', function(done) {
+		MongoClient.connect(`mongodb://127.0.0.1:${mongoPort}`, function(err, client) {
+			if(err) throw err;
+			var collection = client.db(dbname).collection(collectionName);
+			console.log(`Connected to db ${dbname} on port ${mongoPort} for before()`);
+
+			collection.insertOne(exists_in_db.data, function(err, result) {
+				assert.equal(null, err);
+				client.close();
+				done();
+			});
+		});
+
+	});
+
+	after('remove wavy comment from db', function(done) {
+		MongoClient.connect(`mongodb://127.0.0.1:${mongoPort}`, function(err, client) {
+			if(err) throw err;
+			var collection = client.db(dbname).collection(collectionName);
+			console.log(`Connected to db ${dbname} on port ${mongoPort} for after().`);
+
+			collection.deleteOne({ name: exists_in_db.data.name}, function(err, result) {
+				assert.equal(err, null);
+				//assert.equal(1, result.result.n); // TODO may not always hold?	
+				collection.deleteMany({ name: is_wavy.data.name}, function(err, result) {
+					assert.equal(err, null);
+					//assert.equal(1, result.result.n); // TODO may not always hold?
+					client.close();
+					done();
+				});
+			});
+		});
+	});
+
+
+
+	describe('#insertIfValid()', function(done) {
+		it('should return false when comment does not contain wavy emoji', function(done) {
+			mongoHandler().then(function(export_fns) {
+				export_fns.insertIfValid(not_wavy).then( function(b) {
+					assert.ok(!b)
+					done();
+				});
+			}, function(err) {
+				console.log(err);
+		    });
+		});
+		it('should return false because comment exists in db', function(done) {
+			mongoHandler().then(function(export_fns) {
+				var insert_promise = export_fns.insertIfValid(exists_in_db);
+				insert_promise.then(function(b) {
+					assert.ok(!b);
+					done();
+				});
+			}, function(err) {
+				console.log(err);
+			});
+		});
+		it('should add comment when it did not previously exist in db', function(done) {
+			mongoHandler().then(function(export_fns) {
+				
+				export_fns.insertIfValid(is_wavy).then(function(b) {
+					assert.ok(b);
+					done();
+				});
+			}, function(err) {
+				console.error("error: ", err);
+			});
+		});
+	});
+
+	//TODO: retrieveRecent()
+});
---
+++ comment_collector/utils/mongo_handler.js
@@ -0,0 +1,88 @@
+const emoji = require('node-emoji');
+var MongoClient = require('mongodb').MongoClient
+	,format = require('util').format
+	, mongoPort = '27017'
+	, collectionName = 'test'
+	, dbName = 'test'
+	, collection;
+
+function isWavy(commentBody) {
+	const waves = [emoji.get('ocean'), 'wavy', 'wavey'];
+	var isWavy = false;
+
+	for (var i = 0; i < waves.length; i++) {
+		isWavy = ((commentBody.includes(waves[i])) ? true : isWavy);
+	}
+	return isWavy;
+};
+
+function inDatabase(comment_name) {
+	// TODO: replace find with some sort of exist() function? maybe index the db?
+	return collection.find({ name: comment_name}).hasNext();
+};
+
+const export_fns = {
+
+	// Expects a json-parsed object
+	insertIfValid: function(comment) {
+		var contents = comment.data.body;
+		var user = comment.data.author;
+		var comment_name = comment.data.name
+		//if (!contents || !user) {
+		//	throw new // TODO: error, problem w/ comment json, include comment in err
+		//}
+
+
+		// first, check if contains wavyness
+		if (!isWavy(contents)) {
+			// TODO: monitor & log this?
+			return new Promise(function(resolve) { resolve(false);});
+		}
+
+		// second, check if comment exists in db
+		return inDatabase(comment_name).then( function(b) {
+			if (b) return false;
+			return collection.insertOne(comment.data).then(function(insert) {
+				return true;
+			}, function(err) {
+				if (err) {
+					console.error('Error with collection insertion: ',err);
+					return collection.find().toArray()
+					.then( function (array) { 
+						console.log("array: ", array);
+						return false;
+					});
+				}
+			});
+		});
+	},
+
+	retrieveRecent: function(limit=1) {
+		//if (limit > 10) {
+		//	throw new // TODO: limit exceeded error?
+		//}
+		// Will be ordered new -> old
+		return collection.find().sort({created: -1}).limit(limit)
+			.toArray(function(err, docs) {
+				assert.equal(err, null);
+				assert.equal(docs.length, limit); // TODO: should be less than or equal
+			});
+		// return docs;
+	}
+};
+
+var url = `mongodb://127.0.0.1:${mongoPort}`;
+
+//TODO: should take in dbname & collectionName
+module.exports = function() {
+	var promise = MongoClient.connect(url).then(
+		function(client) {
+			collection = client.db(dbName).collection(collectionName);
+			console.log(`Connected to db ${dbName} on port ${mongoPort}.`);
+			return export_fns;
+		}, function(err) {
+			console.log(err)
+		}
+	);
+	return promise;
+}

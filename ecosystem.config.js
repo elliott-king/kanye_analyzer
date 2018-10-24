c73d4031ee2f7d25fd34fe2f6639317b5be23bb5
elliott-king <8884571+elliott-king@users.noreply.github.com>
2018-10-24 22:45:55 +0000

    Add the realtime collector to the pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,74 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'API',
+      script    : 'app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+    // Second application
+    {
+      name      : 'WEB',
+      script    : 'web.js'
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
+
+module.exports = {
+	apps: [
+		{
+			name 	: "hello",
+			script  : "./node_test/hello.js",
+		},{
+			name 	: "kanye_backend",
+			script  : "./kanye_realtime/src/index.js",
+			args    : "--serverPort=8080 --dbname=kanye --collName=wavy-comments",
+		},{
+			name 	: "comment_collector",
+			script  : "./comment_collector/realtime_collector.js",
+		}]
+//		},{
+//			name 		: "realism-dev",
+//			script 		: "./kanye_realtime/kanye_realism/src/index.js",
+//			watch 		: "true",
+//			ignore_watch: ["node_modules"]		
+//		}]
+};
+		

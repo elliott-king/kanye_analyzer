4d6e52584d662f472d9dd580be9aec3a8b049201
elliott-king <8884571+elliott-king@users.noreply.github.com>
2018-10-26 14:26:54 +0000

    Moved fully to React & jsx  -> next to make it pretty

--- kanye_realtime/src/client/app/index.jsx
+++ kanye_realtime/src/client/app/index.jsx
@@ -2,65 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import '../style.css';
 
-class Square extends React.Component {
-	  render() {
-		      return (
-				        <button className="square">
-				  			{/* TODO */}
-				        </button>
-				      );
-		    }
+function Comment(props) {
+	return (
+		<div className="comment" key={props.commentId}>
+			<p className="comment-author">{props.author}</p>
+			<p className="comment-date">{new Date(props.datePosted * 1000).toString()}</p>
+			<p className="comment-body">{props.body}</p>
+		</div>
+	);
 }
 
-class Board extends React.Component {
-	  renderSquare(i) {
-		      return <Square />;
-		    }
+class CommentContainer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			commentQueue: []
+		};
 
-	  render() {
-		      const status = 'Next player: X';
+		// Handle incoming messages.
+		this.sock = io();
+		this.sock.on('connect', () => {
+			console.log('Connected to server');
+		});
+		this.sock.on('comment', (comment) => {
+			this.addComment(JSON.parse(comment));
+		});
+	}
 
-		      return (
-				        <div>
-				          <div className="status">{status}</div>
-				          <div className="board-row">
-				            {this.renderSquare(0)}
-				            {this.renderSquare(1)}
-				            {this.renderSquare(2)}
-				          </div>
-				          <div className="board-row">
-				            {this.renderSquare(3)}
-				            {this.renderSquare(4)}
-				            {this.renderSquare(5)}
-				          </div>
-				          <div className="board-row">
-				            {this.renderSquare(6)}
-				            {this.renderSquare(7)}
-				            {this.renderSquare(8)}
-				          </div>
-				        </div>
-				      );
-		    }
+	renderComment(commentId, author, datePosted, body) {
+		return (
+			<Comment key={commentId} author={author} datePosted={datePosted} body={body}/>
+		);
+	}
+
+	render() {
+		let commentArray = [];
+		for (var i = 0; i < this.state.commentQueue.length; i++){
+			var comment = this.state.commentQueue[i];
+			commentArray.push(this.renderComment(
+				comment.name, 
+				comment.author, 
+				comment.created_utc, 
+				comment.body));
+		}
+		return (
+			<div className="comment-container">
+				{commentArray}
+			</div>
+		)
+	}
+
+	// TODO: hacky
+	addComment(comment) {
+
+		// Keep immutable for React
+		var newCommentQueue;
+
+		// Limit queue to length 5
+		if (this.state.commentQueue.length ===5) {
+			newCommentQueue = this.state.commentQueue.slice(1);
+		}
+		else {
+			newCommentQueue = this.state.commentQueue.slice();
+		}
+
+		newCommentQueue.push(comment);
+		this.setState({commentQueue: newCommentQueue});
+	}
 }
 
-class Game extends React.Component {
-	  render() {
-		      return (
-				        <div className="game">
-				          <div className="game-board">
-				            <Board />
-				          </div>
-				          <div className="game-info">
-				            <div>{/* status */}</div>
-				            <ol>{/* TODO */}</ol>
-				          </div>
-				        </div>
-				      );
-		    }
-}
 
- ReactDOM.render(
-   <Game />,
-     document.getElementById('root')
-     );
+// ReactDOM.render(
+//   <Game />,
+//     document.getElementById('root')
+//     );
      
+ReactDOM.render(
+	<CommentContainer />,
+	document.getElementById('realtime-container')
+);

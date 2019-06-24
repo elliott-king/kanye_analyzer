8bd293991d79b7023ec12d89ed392a531c11a0f8
elliott-king <8884571+elliott-king@users.noreply.github.com>
2019-06-24 11:35:46 -0400

    minor comment cleanup

--- nlp/nlp_test.py
+++ nlp/nlp_test.py
@@ -27,38 +27,9 @@ basic_comment =  {
     'created_utc': 1541259203,
     'name': 't1_e8z9okx'}
 
-not_wavy = {
-    'author': 'test_author_for_node',
-    'name': 'not_wavy_comment',
-    'body': 'does not contain the \'w\' word',
-    'created_utc': 3020
-}
-
-exists_in_db = {
-    'author': 'test_author_for_node',
-    'name': 'exists_in_db_wavy_comment',
-    'body': 'does contain \'wavy\'',
-    'created_utc': 4455
-}
-
-is_wavy = {
-    'author': 'test_author_for_node',
-    'name': 'test_comment_new_in_db',
-    'body': 'does contain wavy',
-    'created_utc': 10
-}
-
 db = mongo_handler.DB_KANYE
 
 class FeatureExtractorTest(unittest.TestCase):
-    # def setUp(self):
-    #     self.client = MongoClient()
-    #     comments_collection = self.client.test[constants.COMMENTS]
-    #     comments_collection.insert_many([ long_comment, basic_comment, not_wavy, exists_in_db, is_wavy ])
-
-    # def tearDown(self):
-    #     comments_collection = self.client.test[constants.COMMENTS]
-    #     comments_collection.delete_many()
 
     def testEmptyCommentExtractor(self):
         with self.assertRaises(ValueError):

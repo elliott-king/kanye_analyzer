22d14f41f5c2ec472b222c58fb916c278d90684a
elliott-king <8884571+elliott-king@users.noreply.github.com>
2019-06-24 11:33:39 -0400

    Add metrics test to mongo handler

--- nlp/mongo_handler_test.py
+++ nlp/mongo_handler_test.py
@@ -45,6 +45,22 @@ is_wavy = {
     'created_utc': 10
 }
 
+# my (official) classifications
+classified_comments = [
+    {'name': 't1_e6oq65l', 'category': 'poster'},
+    {'name': 't1_e6or8v7', 'category': 'op'},
+    {'name': 't1_e6os2j5', 'category': 'link', 'is_wavy': 'wavy'},
+    {'name': 't1_e6oukv2', 'category': 'external_object', 'is_wavy': 'not_wavy'},
+    {'name': 't1_e6pri2s', 'category': 'poster'},
+    {'name': 't1_e6pt2xg', 'category': 'external_person'},
+    {'name': 't1_e6pwf43', 'category': 'external_person'},
+    {'name': 't1_e6pwj5r', 'category': 'kanye'},
+    {'name': 't1_e6px1gk', 'category': 'misc'},
+    {'name': 't1_e6px4pt', 'category': 'link', 'is_wavy': 'wavy'},
+    {'name': 't1_enmx6kt', 'category': 'poster', 'is_wavy': 'wavy'},
+    {'name': 'new_to_db', 'category': 'link', 'is_wavy': 'wavy'}
+]
+
 # User classifications.
 user_classifications = [
     {
@@ -200,6 +216,29 @@ class UserClassificationTest(unittest.TestCase):
         for comment in mongo_handler.get_all_user_classified_comments(db=db):
             if comment['name'] == 'only_classified_category':
                 self.assertDictEqual(comment, {'name': 'only_classified_category', 'category': 'link'})
+
+class MetricsDisplay(unittest.TestCase):
+    def setUp(self):
+        self.client = MongoClient()
+        classified_comments_collection = self.client.test[constants.TRAIN_CATEGORIES]
+        classified_comments_collection.insert_many(classified_comments)
+
+    def tearDown(self):
+        self.client.test[constants.TRAIN_CATEGORIES].delete_many({})
+
+    def testMetricsDisplay(self):
+        metrics = mongo_handler.categories_counts(db=mongo_handler.DB_TEST)
+
+        total_pct = 0
+        total_count = 0
+
+        for category in metrics:
+            count, pct = metrics[category]
+            total_count += count 
+            total_pct += pct 
+        self.assertEqual(total_count, 12)
+        self.assertGreaterEqual(total_pct, 98)
+        self.assertLessEqual(total_pct, 100)
 
 if __name__ == '__main__':
     unittest.main()
